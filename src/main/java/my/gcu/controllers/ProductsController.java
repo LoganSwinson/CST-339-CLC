aa1cff1d6cbfe9e4b574a2616c48ec0c3220d75c
LoganSwinson <97139851+LoganSwinson@users.noreply.github.com>
2024-12-11 02:53:44 -0700

    Fixed DDL Script and moved all API related methods to a controller

--- src/main/java/my/gcu/controllers/ProductsController.java
+++ src/main/java/my/gcu/controllers/ProductsController.java
@@ -1,14 +1,9 @@
 package my.gcu.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-import my.gcu.models.ProductModel;
 import my.gcu.services.LoginService;
 import my.gcu.services.ProductService;
 
@@ -25,15 +20,5 @@ public class ProductsController
         model.addAttribute("isLoggedIn", loginServiceBean.getIsLoggedIn());
         model.addAttribute("productList", productServiceBean.getProductList());
         return "products";
-    }
-
-      @GetMapping("/item/{id}")
-    public ResponseEntity<ProductModel> getProductById(@PathVariable("id") Integer id) {
-        ProductModel product = productServiceBean.getProductById(id);
-        if (product != null) {
-            return new ResponseEntity<>(product, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
     }
 }
